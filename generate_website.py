3ba640247aac980a06b80bcd4a50c95fc787ec03
yuchen-xue <32350820+SeanSyue@users.noreply.github.com>
2025-04-17 21:36:55 +0200

    Build website on a separate branch

--- generate_website.py
+++ generate_website.py
@@ -11,16 +11,16 @@ TOML_FILE = "Yuchen_Xue-data.toml"
 INDEX_TEMPLATE = "index_template.html"
 
 # Path to the output HTML index file
-OUTPUT = "index.html"
+OUTPUT_FOLDER = "_site"
 
 
-def main(toml_file: str, index_template: str, output: str):
+def main(toml_file: str, index_template: str, output_folder: str):
     """Create a website index page from a TOML file and a Jinja2 template.
 
     Args:
         toml_file (str): _path to the TOML file containing website data_
         index_template (str): _path to the Jinja2 template file for the website index page_
-        output (str): _path to the output HTML index file_
+        output_folder (str): _a folder that contains the output HTML index file_
     """
 
     # Load Toml data
@@ -48,8 +48,12 @@ def main(toml_file: str, index_template: str, output: str):
             - Toml file: {toml_file}")
     print("Start rendering HTML output ...")
     
+    # Create the directory that contains the rendered HTML file
+    output_folder = Path(output_folder)
+    output_folder.mkdir(parents=True, exist_ok=True)
+
     # Write the output to an HTML file
-    output_file = Path(output)
+    output_file = output_folder.joinpath("index.html")
     with output_file.open("w", encoding="utf-8") as f:
         f.write(html_output)
 
@@ -57,4 +61,4 @@ def main(toml_file: str, index_template: str, output: str):
 
 
 if __name__ == "__main__":
-    main(TOML_FILE, INDEX_TEMPLATE, OUTPUT)
+    main(TOML_FILE, INDEX_TEMPLATE, OUTPUT_FOLDER)
